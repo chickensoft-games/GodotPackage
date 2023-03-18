bd036ad884f47610ce54c15b0e742f5778335da2
Mike <mikesingleton.24@gmail.com>
2023-03-17 22:08:38 -0230

    commit

--- Chickensoft.GodotPackage.Tests/test/Tests.cs
+++ Chickensoft.GodotPackage.Tests/test/Tests.cs
@@ -8,6 +8,5 @@ public partial class Tests : Node2D {
   /// <summary>
   /// Called when the node enters the scene tree for the first time.
   /// </summary>
-  public override void _Ready()
-	=> GoTest.RunTests(Assembly.GetExecutingAssembly(), this);
+  public override void _Ready() => GoTest.RunTests(Assembly.GetExecutingAssembly(), this);
 }
