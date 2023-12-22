671c0af150ad0572db39b92afc45187d550b4bbe
Joanna May <joannamadisonmay@gmail.com>
2023-12-22 13:38:41 -0600

    fix: misc issues

--- Chickensoft.GodotPackage.Tests/test/src/PackageTest.cs
+++ Chickensoft.GodotPackage.Tests/test/src/PackageTest.cs
@@ -1,7 +1,7 @@
 namespace Chickensoft.GodotPackage.Tests;
 
+using Chickensoft.GoDotTest;
 using Godot;
-using GoDotTest;
 using Shouldly;
 
 public class PackageTest : TestClass {
--- Chickensoft.GodotPackage/src/Package.cs
+++ Chickensoft.GodotPackage/src/Package.cs
@@ -1,5 +1,11 @@
 namespace Chickensoft.GodotPackage;
 
+/// <summary>
+/// Package class.
+/// </summary>
 public class Package {
+  /// <summary>
+  /// Method.
+  /// </summary>
   public string Method() => "Hello, world!";
 }
