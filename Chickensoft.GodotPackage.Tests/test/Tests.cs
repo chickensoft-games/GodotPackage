31df336bbc3570daa7dd7e2231c357109520222d
Mark Wilson <23439518+wlsnmrk@users.noreply.github.com>
2024-04-23 10:25:18 -0400

    Fix build errors for non-Chickensoft packages (#97)
    Fully qualify Chickensoft.GoDotTest in a using statement, so that
    packages created with this tool that are not in the Chickensoft
    namespace will build without errors.

--- Chickensoft.GodotPackage.Tests/test/Tests.cs
+++ Chickensoft.GodotPackage.Tests/test/Tests.cs
@@ -2,7 +2,7 @@ namespace Chickensoft.GodotPackage.Tests;
 
 using System.Reflection;
 using Godot;
-using GoDotTest;
+using Chickensoft.GoDotTest;
 
 public partial class Tests : Node2D {
   public override void _Ready()
